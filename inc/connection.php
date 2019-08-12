4a3150e557ed1b5cebdd741b11ab77e17c71e716
Andrew Hale <andyhalebmx@hotmail.com>
2019-08-12 16:42:08 +0100

    database stuff

--- inc/connection.php
+++ inc/connection.php
@@ -3,10 +3,14 @@
 try {
     // creates new PHPDataObject. sqlite database.
     $db = new PDO("sqlite:".__DIR__."/database.db");
+
     // this will show descriptive error messages
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // echo "Connection successful";
+
     // When using MAMP, the catch block doesn't run
-    // if the database filename is misspelt; it seems to make
+    // if the database filename is misspelt; for some reason, it seems to make
     // a new .db file with the misspelt name instead.
 } catch (Exception $e) {
     echo "Unable to connect";
@@ -14,4 +18,15 @@ try {
     exit;
 }
 
-echo "Connected to the database";
+try {
+    $results = $db->query("SELECT title, category, img FROM Media");
+    echo "Retrieved Results";
+} catch (Exception $e) {
+    echo "Unable to retrieve results";
+    echo $e->getMessage();
+    exit;
+}
+
+//
+
+$catalog = $results->fetchAll(PDO::FETCH_ASSOC);
--- inc/data.php
+++ inc/data.php
@@ -1,4 +1,8 @@
 <?php
+
+// include ("connection.php");
+
+
 $catalog = [];
 // Books
 $catalog[101] = [
@@ -238,51 +242,3 @@ $catalog[311] = [
 ];
 
 ?>
-
-
-<!-- $catalog = [];
-$catalog[101] = [
-    "artist" => "Holy Other",
-    "title" => "Held",
-    "img" => "img/media/held.jpg",
-    "genre" => "Electronic",
-    "year" => "2012",
-    "category" => "Music",
-    "label" => "Tri-Angle Records"
-];
-$catalog[102] = [
-    "artist" => "Holy Other",
-    "title" => "With U",
-    "img" => "img/media/with_u.jpg",
-    "genre" => "Electronic",
-    "year" => "2011",
-    "category" => "Music",
-    "label" => "Tri-Angle Records"
-];
-$catalog[103] = [
-    "artist" => "Burial",
-    "title" => "Burial",
-    "img" => "img/media/burial.jpg",
-    "genre" => "Electronic",
-    "year" => "2006",
-    "category" => "Music",
-    "label" => "Hyperdub"
-];
-$catalog[104] = [
-    "artist" => "Burial",
-    "title" => "Untrue",
-    "img" => "img/media/untrue.jpg",
-    "genre" => "Electronic",
-    "year" => "2007",
-    "category" => "Music",
-    "label" => "Hyperdub"
-];
-$catalog[105] = [
-    "artist" => "Clams Casino",
-    "title" => "Rainforest",
-    "img" => "img/file_icon.png",
-    "genre" => "Electronic",
-    "year" => "2007",
-    "category" => "Music",
-    "label" => "Tri-Angle Records"
-]; -->
